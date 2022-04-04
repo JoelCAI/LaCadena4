bb6cb51ad902357be173006ea7fcba308647a26b
JoelCAI <joelargentina12345@gmail.com>
2022-04-04 01:20:20 -0300

    Agregar archivos de proyecto.

---
+++ LaCadena4/Sistema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena4
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            string cadena;
+
+            cadena = Validador.ValidarString("\n Ingrese una frase");
+
+            Validador.ToLower(cadena);
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ LaCadena4/Validador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena4
+{
+    public class Validador
+    {
+        public static void ToLower(string cadena)
+        {
+            string opcion = cadena.ToLower();
+            Console.WriteLine("\n Usted escribio " + "*" + cadena + "*" + "\n\n Ahora su frase es: " + "*" + opcion + "*");
+
+        }
+
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+        public static string ValidarString(string mensaje)
+        {
+
+            string opcion;
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                opcion = Console.ReadLine();
+                if (string.IsNullOrEmpty(opcion))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" \nEl valor ingresado no puede ser vacio.");
+                    Console.WriteLine(" Por favor ingrese un valor.");
+                }
+            } while (string.IsNullOrEmpty(opcion));
+            return opcion;
+
+        }
+    }
+}
